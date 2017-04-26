e5ec956d84ca36931b627698f039262f3d7f034b
mridul <mridulkapoor@gmail.com>
2017-04-25 23:10:46 -0400

    change frameset conversion

--- vgg_videos.py
+++ vgg_videos.py
@@ -28,15 +28,40 @@ def forward_prop_and_save(model, X, dest_path):
 def featurize_dir(source_dir):
     subdirs = [d for d in os.listdir(source_dir) if os.path.isdir(os.path.join(source_dir, d))]
     target_size = (150, 150)
+    required_count = 20
     X = []
     for d in subdirs:
         frameset = []
         dirpath = os.path.join(source_dir, d)
-        for f in glob(dirpath + '/*.jpg'):
-            img = imread(f)
+
+        images = os.listdir(dirpath)
+        images = [im for im in images if im.endswith('.jpg')]
+        images = images[:required_count]
+
+        for f in images:
+            full_path = os.path.join(dirpath, f)
+            img = imread(full_path)
             img = imresize(img, target_size)
             img = np.asarray(img)
             frameset.append(img)
+
+        frameset_length = len(frameset)
+        if frameset_length < required_count:
+            ipdb.set_trace()
+            diff = required_count - frameset_length
+            half_diff = (diff * 1.0)/2
+
+            start_rep = int(math.floor(half_diff))
+            end_rep = int(math.ceil(half_diff))
+            start_padding = [frameset[0]] * start_rep
+            end_padding = [frameset[-1]] * end_rep
+
+            new_frameset = []
+            new_frameset.extend(start_padding)
+            new_frameset.extend(frameset)
+            new_frameset.extend(end_padding)
+
+            frameset = new_frameset
             
         frameset = np.asarray(frameset)
         X.append(frameset)
