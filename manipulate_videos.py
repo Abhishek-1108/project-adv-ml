62f498747233501c14b7a88684ee4f5d0c22b18f
mridul <mridulkapoor@gmail.com>
2017-04-25 22:03:56 -0400

    create frames directly from the large video

--- manipulate_videos.py
+++ manipulate_videos.py
@@ -34,6 +34,7 @@ def cut_videos(vid_segments_file, downloads_dir, destination_dir):
     with open(vid_segments_file) as infile:
         lines = [l for l in infile]
 
+    # parse file
     for line in lines:
         if line.startswith('#'):
             continue
@@ -42,61 +43,65 @@ def cut_videos(vid_segments_file, downloads_dir, destination_dir):
         ytid, start, end = split_line[0], split_line[1], split_line[2]
         segments[ytid] = (start, end)
 
+    # use downloads to create frames
     for f in glob(downloads_dir + '/*.mp4'):
         dest_path = f.replace(downloads_dir, destination_dir)
+
         ytid = os.path.splitext(os.path.basename(f))[0]
+        dest_path = os.path.join(dest_path, ytid)
+        make_dir_if_not_exist(dest_path)
+
         start_time = int(float(segments[ytid][0].strip()))
-        start_time = seconds_to_hms(start_time)
         end_time = int(float(segments[ytid][1].strip()))
-        end_time = seconds_to_hms(end_time)
 
-        if os.path.exists(dest_path):
-            print('skipped {}. segment exists.'.format(f))
-        else:
-            command = 'ffmpeg -i {input} -ss {start} -to {end} -c copy {output}'.format(
-                start=start_time,
-                end=end_time,
-                input=f,
-                output=dest_path
-            )
-            print(command)
-            os.system(command)
+        full_filepath = os.path.join(downloads_dir, f)
+        write_frames_to_dir(full_filepath, dest_path, start_time, end_time)
 
 
-def write_frames_to_dir(vid_file, dest_dir):
+def write_frames_to_dir(vid_file, dest_dir, start_time, end_time):
     # minimum and maximum fps was ~6 and ~30 respectively 
     # for the first set of filtered videos
     # lets average the frames after every 0.5 seconds (approx.)
     vid = cv2.VideoCapture(vid_file)
     fps = round(vid.get(cv2.CAP_PROP_FPS), ndigits=0)
-
-    # how many frames fit in our window size of 0.5 seconds
+    print('fps', fps)
     window_size = int(fps / 2)
+    print('window_size', window_size)
 
-    i = -1
-    frame_number = 0
+    start_frame_count = fps * start_time
+    stop_frame_count = fps * end_time
+
+    frame_number = -1
+    averaged_frame_count = -1
+    window_count = 0
     window = []
     while True:
         read_success, frame = vid.read()
         if read_success:
             frame_number += 1
-            window.append(frame)
-            if frame_number == window_size:
-                window_average = np.average(np.asarray(window), axis=0)
-                i += 1
-                dest_path = '{}/{}.jpg'.format(dest_dir, i)
-                cv2.imwrite(dest_path, window_average)
-                window = []
-                frame_number = 0
+
+            # video segment of interest starts here
+            if start_frame_count < frame_number:
+                if frame_number < stop_frame_count:
+                    window_count += 1
+                    window.append(frame)
+                    # if our window is full, average it and flush out
+                    if window_count == window_size:
+                        window_average = np.average(np.asarray(window), axis=0)
+                        averaged_frame_count += 1
+                        dest_path = '{}/{}.jpg'.format(dest_dir, averaged_frame_count)
+                        cv2.imwrite(dest_path, window_average)
+                        window = []
+                        window_count = 0
+                else:
+                    # video segment of interest has ended
+                    # average and flush out any remaining part of the window
+                    if len(window) > 0:
+                        window_average = np.average(np.asarray(window), axis=0)
+                        averaged_frame_count += 1
+                        dest_path = '{}/{}.jpg'.format(dest_dir, averaged_frame_count)
+                        cv2.imwrite(dest_path, window_average)
         else:
-            # no more frames to read
-            # flush out the last remanants of the video and return
-            if len(window) > 0:
-                window_average = np.average(np.asarray(window), axis=0)
-                i += 1
-                dest_path = '{}/{}.jpg'.format(dest_dir, i)
-                cv2.imwrite(dest_path, window_average)
-            
             return
 
 
@@ -116,21 +121,22 @@ def create_frames(video_src_dir, dest_frames_dir):
 
 def main():
     vid_segments_info_file = os.getenv('filtered_path')
-    
+
     downloads_dir = os.getenv('downloads_dir')
-    segments_dir = os.getenv('segments_dir')
-    make_dir_if_not_exist(segments_dir)
+    # segments_dir = os.getenv('segments_dir')
+    # make_dir_if_not_exist(segments_dir)
+    frames_dir = os.getenv('frames_dir')
+    make_dir_if_not_exist(frames_dir)
     cut_videos(
         vid_segments_file=vid_segments_info_file,
         downloads_dir=downloads_dir,
-        destination_dir=segments_dir    
+        destination_dir=frames_dir
     )
 
-    frames_dir = os.getenv('frames_dir')
-    create_frames(
-        video_src_dir=segments_dir,
-        dest_frames_dir=frames_dir
-    )
+    # create_frames(
+    #     video_src_dir=downloads_dir,
+    #     dest_frames_dir=frames_dir
+    # )
 
 
 if __name__ == '__main__':
